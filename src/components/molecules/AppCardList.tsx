e2be9df6235ab44b28ff750b4aa97c44c5807a0b
Toshimitsu Watanabe <kz86nu@gmail.com>
2022-01-27 12:14:18 +0900

    Disable private Card

--- src/components/molecules/AppCardList.tsx
+++ src/components/molecules/AppCardList.tsx
@@ -1,5 +1,4 @@
 import Grid from "@mui/material/Grid";
-import Box from "@mui/system/Box";
 import { AppCardProps, AppCard } from "../atoms/AppCard";
 import { AppListType } from "utils/types";
 
@@ -20,23 +19,27 @@ export const AppCardList = ({
     </Grid>
   );
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid
-        container
-        columns={{ xs: 2, md: 3, lg: 4, xl: 5 }}
-        spacing={{ xs: 1, md: 2, lg: 4 }}
-      >
-        {publicOnly
-          ? Object.values(apps)
-              .filter(
-                (app) =>
-                  app.location === location && app.visibility === "public"
-              )
-              .map((app) => <GridAppCard key={app.id} app={app} />)
-          : Object.values(apps)
-              .filter((app) => app.location === location)
-              .map((app) => <GridAppCard key={app.id} app={app} />)}
-      </Grid>
-    </Box>
+    <Grid
+      container
+      columns={{ xs: 2, md: 3, lg: 4, xl: 5 }}
+      spacing={{ xs: 1, md: 2, lg: 4 }}
+      sx={{ flexGrow: 1, width: "100%" }}
+    >
+      {publicOnly
+        ? Object.values(apps)
+            .filter((app) => app.location === location)
+            .map((app) => (
+              <GridAppCard
+                key={app.id}
+                app={{
+                  ...app,
+                  disable: app.visibility !== "public",
+                }}
+              />
+            ))
+        : Object.values(apps)
+            .filter((app) => app.location === location)
+            .map((app) => <GridAppCard key={app.id} app={app} />)}
+    </Grid>
   );
 };
